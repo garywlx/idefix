fbe14ea0e16c7afaf8c29fa70737f42923b1df35
Arne Gockeln <arne@gockeln.com>
2018-07-22 12:14:25 +0200

    final: majors pip value in USD and EUR account

--- docs/formula.md
+++ docs/formula.md
@@ -54,7 +54,64 @@ $$
 
 
 
-## Profit Loss Calculation
+### Pip Value Calculation When Trading a USD Account
+
+When the USD is listed second in a pair the pip value is fixed and doesn't change, assuming you have a USD dollar account.
+
+The fixed pip amount is:
+
+- $10 for a standard lot which is 100,000 worth of currency.
+- $1 for a mini lot which is 10,000 worth of currency.
+- $0.10 for a micro lot is 1,000 worth of currency.
+
+These pip values apply to any pair where the USD is listed second, such as the EUR/USD, [GBP/USD](https://www.thebalance.com/best-time-to-day-trade-the-gbp-usd-forex-pair-1031020), AUD/USD, NZD/USD
+
+If the USD isn't listed second:
+
+- Divide the pip values above by the USD/XXX rate. 
+
+For example, to get the pip value of a standard lot for the USD/CAD, when trading a USD account, divide \$10 by the USD/CAD rate. If the USD/CAD rate is 1.2500 the standard lot pip value in USD is \$8, or \$10 divided by 1.25.
+
+
+
+### Pip Value Calculation for a Non-USD Account
+
+Whatever currency the account is, when that currency is listed second in a pair the pip values are fixed. 
+
+For example, if you have a Canadian dollar (CAD) account, any pair that is XXX/CAD, such as the USD/CAD will have a fixed pip value. A standard lot is CAD\$10, a mini lot is CAD\$1, and a micro lot is CAD\$0.10.
+
+To find the value of a pip when the CAD is listed first, divide the fixed pip rate by the exchange rate. For example, to find the value of a mini lot, if the CAD/CHF exchange rate is 0.7820, a pip is worth CAD$1.27.
+If the pair includes the [JPY](https://www.thebalance.com/best-time-to-day-trade-the-usd-jpy-forex-pair-1031021), for example the JPY/CAD, then multiply the result by 10. For example, if the CAD/JPY is priced at 89.09, to find out the standard pip value divide CAD\$10 by 89.09, then multiply the result by 10, for a pip value of CAD\$11.23.
+
+Go through this process with any account currency to find pip values for pairs that include that currency.
+
+
+
+### Pip Value For Other Currency Pairs
+
+Not all currency pairs include your account currency. You may have a USD account, but want to trade the EUR/GBP. How's how to figure out the pip value for pairs that don't include your account currency. 
+
+The second currency is always fixed if a person had an account in that currency. For example, we know that if a person held a GBP account then the EUR/GBP pip value is GBP10 for a standard lot, as discussed above. The next step is converting GBP10 to our own currency. If our account is USD, divide GBP10 by the USD/GBP rate. If the rate is 0.7600, then the pip value is USD$13.16.
+
+If you can only find a "backward" quote, such as the GBP/USD rate being 1.3152, then divide one by the rate to get 0.7600. That is the USD/GBP rate. You can then do the calculation above. 
+
+If your account currency is euros and you want to know the pip value of the AUD/CAD, remember that for a person with a CAD account a standard lot would be CAD$10 for this pair. Convert that CAD$10 to euros by dividing it by the EUR/CAD rate. If the rate is 1.4813, the standard lot pip value is EUR6.75.
+
+Always consider which currency is providing the pip value: the second currency (YYY). Once you know that, convert the fixed pip value in that currency to your own by dividing it by XXX/YYY, where XXX is your own account currency. 
+
+
+
+## Pip Value Calculation USD/JPY in EUR account
+
+$$
+pipV = (symbolPointSize * positionQty ) \div snapshotEurJpyAsk
+$$
+
+The same as in PipValue Calculation but with EUR/JPY as pair to calculate.
+
+
+
+## (A) Profit Loss Calculation
 
 Step 1: get pip difference of current bid price and position entry price.
 
@@ -85,32 +142,35 @@ $$
 
 
 
+## Profit Loss USD/JPY Calculation
 
+Step 1 as descibed in A.
 
-## Convert Price to USD
+Step 2: 
 
+Formular for USD account:
 $$
-usdV = otherCurrencyValue \div usdPrice
+profitLoss = ( ( pipDiff / snapshotBidPrice ) * positionQty )
 $$
-
-- **otherCurrencyValue** the value to convert
-- **usdPrice** the current usd price.
-
-
-
-
-
-
-
-## Andre Hedging Formel
-
+Formular for EUR account:
 $$
-pnlVLong = (( currentAsk - entry ) * pipV * 100 ) * ( qty / 100000 )\\
-pnlVShort = (( entry - currentBid ) * pipV * 100 ) * ( qty / 100000 )
+profitLoss = ( (pipDiff / eurjpyBidPrice ) * positionQty )
 $$
 
-- **currentAsk** this is the ask value of the traded pair 
-- **entry** this is the entry price of the position
-- **pipV** this is the pip value for the traded pair
-- **qty** this is the position size
+* **eurjpyBidPrice** the snapshot for EUR/JPY to convert to EUR account.
+
+
+
+## Profit Loss Calculation for NON EUR Pairs in EUR Account
+
+Pairs like GBP/USD needs to be converted.
+
+Step 1 as described in A.
+
+Step 2:
+$$
+profitLoss = (( pipDiff / conversionPrice ) * positionQty ) \div snapshotBidPrice
+$$
+
+* **conversionPrice** The price to convert to. e.g. for GBP/USD use EUR/GBP Bid as conversion price.
 
--- playground_src/main.cpp
+++ playground_src/main.cpp
@@ -8,6 +8,7 @@
 #include <ostream>
 #include <iomanip>
 #include <cmath>
+#include <map>
 
 using namespace std;
 
@@ -78,21 +79,57 @@ std::ostream& operator<<(std::ostream& out, const Account& a) {
 
 /*!
  * Pip Value calculation for quote currency
+ * https://www.myfxbook.com/forex-calculators/pip-calculator
+ * https://www.thebalance.com/calculating-pip-value-in-forex-pairs-1031022
  * 
  * @param const Symbol      snapshot         The current market snapshot.
  * @param const double      pos_qty          The current position qty.
- * @param const std::string account_currency The account currency in capital letters.
+ * @param const std::string account_currency The account currency in capital letters. Default is USD
+ * @param const double      conversion_price The conversion_price for snapshot if it does not contain account_currency. eg. ACC=USD, Snapshot=EUR/GBP => USD/GBP
  * @return double
  */
-double get_pip_value(const Symbol snapshot, const double pos_qty, const std::string account_currency) {
-	// PipValue = (Pip in decimal places * Trade Size) / Market Price
+double get_pip_value(const Symbol snapshot, const double pos_qty, const std::string account_currency = "USD", const double conversion_price = 0) {
+	
+	// Whatever currency the account is, when that currency is listed second in a pair the pip values are fixed. 
+	// For example, if you have a Canadian dollar (CAD) account, any pair that is XXX/CAD, such as the USD/CAD 
+	// will have a fixed pip value. A standard lot is CAD$10, a mini lot is CAD$1, and a micro lot is CAD$0.10.
 	double pip_v = ( snapshot.point_size * pos_qty );
 
-	// if account is denominated in EUR, devide by ask price
-	// OR if symbol base is USD
-	if ( account_currency == "EUR" || ( snapshot.base_currency == "USD" && account_currency == "USD" ) ) {
-		pip_v /= snapshot.ask;
+	// if the account currency is USD
+	if ( account_currency == "USD" ) {
+		// if the USD isn't listed second,
+		// divide the pip value by the USD/xxx rate
+		if ( snapshot.base_currency == "USD" ) {
+			pip_v /= snapshot.ask;	
+		}
+
+		// if the snapshot does not contain account currency
+		// e.g. Account USD && Symbol EUR/CHF
+		if ( snapshot.base_currency != account_currency && snapshot.quote_currency != account_currency ) {
+			// convert with conversion price
+			if ( conversion_price > 0 ) {
+				pip_v /= conversion_price;
+			}
+		}
 	}
+	// account currency is EUR
+	else if ( account_currency == "EUR" ) {
+		// if symbol base equals account currency: EUR/... in EUR account
+		if ( snapshot.base_currency == account_currency && snapshot.quote_currency != "USD" ) {
+			pip_v /= snapshot.ask;
+		}
+
+		// if symbol base equals usd in euro account: USD/... in EUR account
+		if ( snapshot.base_currency == "USD" && account_currency == "EUR" && conversion_price > 0 ) {
+			pip_v /= conversion_price;
+		}
+	}
+
+	// if account is denominated in EUR, devide by ask price
+	// OR if symbol base is USD and account is USD
+	// if ( account_currency == "EUR" || ( snapshot.base_currency == "USD" && account_currency == "USD" ) ) {
+	// 	pip_v /= snapshot.ask;
+	// }
 
 	return pip_v;
 }
@@ -126,9 +163,27 @@ double get_profit_loss(const Symbol snapshot, const Position pos, const std::str
 	if ( snapshot.symbol == "USD/JPY" ) {
 		profit_loss = ( ( pip_diff_decimal / ( account_currency == "EUR" ? conversion_price : snapshot.bid ) ) * pos.qty );
 	} 
-	// other pairs
+	// Account = EUR && GBP/USD
+	/*else if ( account_currency == "EUR" && snapshot.quote_currency == "USD" && snapshot.base_currency != "EUR" ) {
+		profit_loss = ( ( pip_diff_decimal / conversion_price ) * pos.qty ) / snapshot.bid;
+	} */
+	// other pairs with USD/... || .../USD
 	else {
-		profit_loss = ( pip_diff_decimal * pos.qty ); // USD	
+
+		// check for quote .../USD
+		if ( snapshot.quote_currency == "USD" && snapshot.base_currency != "EUR" ) {
+			profit_loss = ( ( pip_diff_decimal / conversion_price ) * pos.qty );
+			cout << ".../USD" << endl;
+		} 
+		// USD/...
+		else if ( snapshot.quote_currency != "USD" && snapshot.base_currency == "USD" ) {
+			profit_loss = ( pip_diff_decimal * pos.qty );
+			cout << "USD/..." << endl;
+			cout << pip_diff_decimal << " " << pos.qty << endl;
+		}
+		else {
+			profit_loss = ( pip_diff_decimal * pos.qty );
+		}
 
 		// if account is denominated in EUR, divide by currency_rate
 		if ( account_currency == "EUR" ) {
@@ -146,33 +201,95 @@ int main(int argc, char** argv){
 	cout << endl;
 
 	// The account
-	Account account = {"EUR", 1000, 0};
+	Account account = {"USD", 1000, 0};
+
+	// set account_currency by argument
+	if ( argc == 2 ) {
+		account.currency = argv[1];
+	}
+
 	cout << account << endl;
 
-	// this symbol is traded
-	Symbol snapshot_eurusd = {"EUR/USD", "EUR", "USD", 1.1721, 1.17211, 0.0001, 5, 100000};
-	Symbol snapshot_eurjpy = {"EUR/JPY", "EUR", "JPY", 130.624, 130.625, 0.01, 4, 100000};
-	Symbol snapshot = {"USD/JPY", "USD", "JPY", 111.459, 111.460, 0.01, 3, 100000};
-	//Symbol snapshot = snapshot_eurusd;
+	
+	// Exchange Symbols for EUR account
+	Symbol eurjpy = {"EUR/JPY", "EUR", "JPY", 130.624, 130.624, 0.01, 4, 100000};
+	Symbol eurgbp = {"EUR/GBP", "EUR", "GBP", 0.89254, 0.89254, 0.0001, 5, 100000};
+	Symbol eurchf = {"EUR/CHF", "EUR", "CHF", 1.16319, 1.16319, 0.0001, 5, 100000};
+	Symbol eurcad = {"EUR/CAD", "EUR", "CAD", 1.53933, 1.53933, 0.0001, 5, 100000};
+
+	// MAJORS
+	Symbol audusd = {"AUD/USD", "AUD", "USD", 0.74202, 0.74212, 0.0001, 5, 100000};
+	Symbol eurusd = {"EUR/USD", "EUR", "USD", 1.17206, 1.17216, 0.0001, 5, 100000};
+	Symbol gbpusd = {"GBP/USD", "GBP", "USD", 1.31307, 1.31317, 0.0001, 5, 100000};
+	Symbol nzdusd = {"NZD/USD", "NZD", "USD", 0.68085, 0.68095, 0.0001, 5, 100000};
+	Symbol usdcad = {"USD/CAD", "USD", "CAD", 1.31325, 1.31335, 0.0001, 5, 100000};
+	Symbol usdchf = {"USD/CHF", "USD", "CHF", 0.99242, 0.99252, 0.0001, 5, 100000};
+	Symbol usdjpy = {"USD/JPY", "USD", "JPY", 111.445, 111.446, 0.01, 3, 100000};
+
+	typedef std::pair<std::string, Symbol> major_pair;
+	std::map<std::string, Symbol> majors;
+
+	majors.insert( major_pair(eurusd.symbol, eurusd) );
+	majors.insert( major_pair(usdjpy.symbol, usdjpy) );
+	majors.insert( major_pair(gbpusd.symbol, gbpusd) );
+	majors.insert( major_pair(usdchf.symbol, usdchf) );
+	majors.insert( major_pair(audusd.symbol, audusd) );
+	majors.insert( major_pair(usdcad.symbol, usdcad) );
+	majors.insert( major_pair(nzdusd.symbol, nzdusd) );
+
+	// Traded symbol
+	Symbol snapshot = eurusd;
 	cout << snapshot << endl;
 
 	// Position
-	//Position pos = {Side::BUY, 100000, 1.1719, 0, 0};
-	Position pos = {Side::BUY, 100000, 111.458, 0, 0};
-	cout << pos << endl;
+	Position position = {Side::BUY, 100000, snapshot.bid - snapshot.point_size, 0, 0};
+	cout << position << endl;
 
 	// PipValue
 	double pip_value = 0;
-	if ( account.currency == "EUR" && snapshot.quote_currency == "JPY" ) {
-		pip_value = get_pip_value( snapshot_eurjpy, pos.qty, account.currency );
-	} else {
-		pip_value = get_pip_value( snapshot, pos.qty, account.currency );	
+	double conversion_price = 0;
+
+	// IMPORTANT FOR EUR ACCOUNT
+	if ( account.currency == "EUR" ) {
+		for ( auto it = majors.begin(); it != majors.end(); ++it ) {
+			// AUD/USD, EUR/USD, GBP/USD, NZD/USD, ...
+			if ( it->second.quote_currency == "USD" ) {
+				pip_value = get_pip_value( it->second, position.qty, account.currency );
+			} 
+			// USD/CAD, USD/CHF, USD/JPY, ...
+			else if ( it->second.base_currency == "USD" ) {
+
+				// get conversion rate
+				conversion_price = 1 / eurusd.ask;
+
+				if ( it->second.quote_currency == "CAD" ) {
+					conversion_price = eurcad.ask;
+				} else if ( it->second.quote_currency == "CHF" ) {
+					conversion_price = eurchf.ask;
+				} else if ( it->second.quote_currency == "JPY" ) {
+					conversion_price = eurjpy.ask;
+				}
+
+				pip_value = get_pip_value( it->second, position.qty, account.currency, conversion_price );
+			}
+
+			cout << it->first << " pip_v " << pip_value << " " << account.currency << endl;
+		}
+	} 
+	// USD ACCOUNT
+	else {
+		for ( auto it = majors.begin(); it != majors.end(); ++it ) {
+			pip_value = get_pip_value( it->second, position.qty, account.currency );
+			cout << it->first << " pip_v " << pip_value << " " << account.currency << endl;
+		}
+		
 	}
-	cout << " pip_v         " << pip_value << " " << account.currency << endl;
-	
+	//cout << " pip_v         " << pip_value << " " << account.currency << endl;
+	//cout << " conversion_p  " << conversion_price << endl;
+
 	// Profit/Loss Value
-	const double pnl = get_profit_loss( snapshot, pos, account.currency, ( account.currency == "EUR" && snapshot.quote_currency == "JPY" ? snapshot_eurjpy.bid : 0 ) );
-	cout << std::setprecision(2) << fixed;
+	const double pnl = get_profit_loss( snapshot, position, account.currency, conversion_price );
+	cout << std::setprecision(5) << fixed;
 	cout << " profit_loss   " << pnl << " " << account.currency << endl;
 
 	cout << endl;
