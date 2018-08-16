12cbdba03593cecdf70b1d02f54e948e753f2885
Arne Gockeln <arne@gockeln.com>
2018-08-16 22:29:41 +0200

    add: operators for strategies and static console implementation

---
+++ src/Console.h
@@ -0,0 +1,21 @@
+#ifndef IDEFIX_CONSOLE_H
+#define IDEFIX_CONSOLE_H
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/daily_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace IDEFIX {
+	inline void registerConsole() {
+		// setup console
+		auto console = spdlog::stdout_color_mt( "console" );
+		// set console pattern
+		console->set_pattern( "%Y-%m-%d %T.%e: %^%v%$" );
+	}
+
+	inline std::shared_ptr<spdlog::logger> console() {
+		return spdlog::get("console");
+	}
+};
+
+#endif
---
+++ src/Operators.h
@@ -0,0 +1,60 @@
+#ifndef IDEFIX_OPERATORS_H
+#define IDEFIX_OPERATORS_H
+
+#include "indicator/RenkoBrick.h"
+
+namespace IDEFIX {
+	/*!
+	 * Check if value is above the line value
+	 * 
+	 * @param const double  value      
+	 * @param const double  line_value 
+	 * @return bool if value > line_value
+	 */
+	bool above(const double value, const double line_value) {
+		return value > line_value;
+	}
+
+	/*!
+	 * Check if brick.open_price and brick.close_price are above line_value
+	 * @param const RenkoBrick&  brick
+	 * @param const double       line_value 
+	 * @return bool if brick.open_price > line_value && brick.close_price > line_value
+	 */
+	bool above(const RenkoBrick& brick, const double line_value) {
+		return ( brick.open_price > line_value && brick.close_price > line_value );
+	}
+
+	/*!
+	 * Check if value is below the line value
+	 * 
+	 * @param const double  value      
+	 * @param const double  line_value 
+	 * @return bool if value < line_value
+	 */
+	bool below(const double value, const double line_value) {
+		return value < line_value;
+	}
+
+	/*!
+	 * Check if brick.open_price and brick.close_price are below line_value
+	 * @param  brick      [description]
+	 * @param  line_value [description]
+	 * @return            [description]
+	 */
+	bool below(const RenkoBrick& brick, const double line_value) {
+		return ( brick.open_price < line_value && brick.close_price < line_value );
+	}
+
+	/*!
+	 * Check if lvalue equals rvalue
+	 * @param const double  left_value 
+	 * @param const double  right_value
+	 * @return bool if rvalue == rvalue
+	 */
+	bool equals(const double lvalue, const double rvalue) {
+		return lvalue == rvalue;
+	}
+};
+
+#endif
