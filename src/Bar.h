366ecb18911df2855d41925aed73ff94b95b5110
Arne Gockeln <arne@gockeln.com>
2018-09-20 12:07:34 +0200

    fix: g++ building

--- src/Bar.h
+++ src/Bar.h
@@ -39,32 +39,32 @@ namespace IDEFIX {
 			period      = 0;
 			point_size  = 0;
 		}
-	};
 
-	inline std::ostream& operator<<(std::ostream& out, const IDEFIX::Bar& brick) {
-		std::string status = "NOSTATUS";
-		bool is_long = true;
-		if ( brick.status == Bar::STATUS::LONG ) {
-			status = "L";
-		} else if ( brick.status == Bar::STATUS::SHORT ) {
-			status = "S";
-			is_long = false;
-		}
+		friend std::ostream& operator<<(std::ostream& out, const Bar& brick) {
+			std::string status = "NOSTATUS";
+			bool is_long = true;
+			if ( brick.status == Bar::STATUS::LONG ) {
+				status = "L";
+			} else if ( brick.status == Bar::STATUS::SHORT ) {
+				status = "S";
+				is_long = false;
+			}
 
-		//out << "\033[" << ( brick.status == Bar::STATUS::SHORT ? "31" : "32" ) << "m";
-		out << "Brick " << status << " " << brick.open_time << " ";
-		out << std::setprecision(5) << std::fixed; 
-		out << brick.open_price << " ";
-		out << std::setprecision(5) << std::fixed; 
-		out << brick.high_price << " ";
-		out << std::setprecision(5) << std::fixed; 
-		out << brick.low_price << " ";
-		out << std::setprecision(5) << std::fixed; 
-		out << brick.close_price << " ";
-		out << brick.close_time << " ";
-		out << brick.diff << " " << brick.volume;
-		//out << "\033[0m";
-		return out;
+			//out << "\033[" << ( brick.status == Bar::STATUS::SHORT ? "31" : "32" ) << "m";
+			out << "Brick " << status << " " << brick.open_time << " ";
+			out << std::setprecision(5) << std::fixed; 
+			out << brick.open_price << " ";
+			out << std::setprecision(5) << std::fixed; 
+			out << brick.high_price << " ";
+			out << std::setprecision(5) << std::fixed; 
+			out << brick.low_price << " ";
+			out << std::setprecision(5) << std::fixed; 
+			out << brick.close_price << " ";
+			out << brick.close_time << " ";
+			out << brick.diff << " " << brick.volume;
+			//out << "\033[0m";
+			return out;
+		};
 	};
 };
 
